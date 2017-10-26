939157d37c03945d7007a3a70465ad4b41e4ba67
yamara <yklsga@gmail.com>
2017-10-25 23:22:16 -0400

    added request

--- app.py
+++ app.py
@@ -1,35 +1,3 @@
-# from flask import Flask, Response, request
-# from twilio import twiml
-
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def check_app():
-#     # returns a simple string stating the app is working
-#     return Response("It works!"), 200
-
-
-# @app.route("/twilio", methods=["POST"])
-# def inbound_sms():
-#     response = twiml.Response()
-#     # we get the SMS message from the request. we could also get the 
-#     # "To" and the "From" phone number as well
-#     inbound_message = request.form.get("Body")
-#     # we can now use the incoming message text in our Python application
-#     if inbound_message == "Hello":
-#         response.message("Hello back to you!")
-#     else:
-#         response.message("Hi! Not quite sure what you meant, but okay.")
-#     # we return back the mimetype because Twilio needs an XML response
-#     return Response(str(response), mimetype="application/xml"), 200
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -38,9 +6,11 @@ app = Flask(__name__)
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
+    a = request.form.get('Body')
+    print(a)
     # Start our TwiML response
     resp = MessagingResponse()
-
+    print(resp)
     # Add a message
     resp.message("The Robots are coming! Head for the hills!")
 
