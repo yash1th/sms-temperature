b391c991caa3051aed44db644df6a0d7ae80d110
yamara <yklsga@gmail.com>
2017-11-09 14:58:57 -0800

    modified message template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
-from get_weather import get_current_weather_by_location, get_forecast_weather_by_location
+from get_weather import get_weather_by_location
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def sms_reply():
     else:
         location = request.form.get('Body')
         resp = MessagingResponse()
-        resp.message(get_current_weather_by_location(location)+get_forecast_weather_by_location(location))
+        resp.message(get_weather_by_location(location.title()))
         return str(resp)
 
 if __name__ == "__main__":
--- get_weather.py
+++ get_weather.py
@@ -3,7 +3,7 @@ import json
 import configparser
 import sys
 import os
-
+from pprint import pprint as pp
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
@@ -17,7 +17,7 @@ def get_response(url, parameters):
     return r
 
 def get_temperature_summary(temperature_details):
-    current_temperature = 'now: '+str(temperature_details['temp'])
+    current_temperature = 'temp: '+str(temperature_details['temp'])
     maximum_temperature = 'high: '+str(temperature_details['temp_max'])
     minimum_temperature = 'low: '+str(temperature_details['temp_min'])
     if temperature_details['temp_max'] == temperature_details['temp_min']:
@@ -30,7 +30,7 @@ def get_message_string(data):
     s['header'] = data['name']+', '+data['sys']['country']+'-'
     s['summary'] = data['weather'][0]['description']
     s['temperature'] = get_temperature_summary(data['main'])
-    s['windspeed'] = 'wind speed : '+ str(data['wind']['speed']) + ' miles/hour'
+    s['windspeed'] = 'wind: '+ str(data['wind']['speed']) + ' m/h'
     message= '\n'.join((s['header'], s['summary'], s['temperature'], s['windspeed']))
     return message
 
@@ -45,11 +45,10 @@ def get_current_weather_by_location(message):
 
 def get_forecast_string(data):
     s = {}
-    s['header'] = '\nforecast for the next 3 hours'
+    s['header'] = '\n3 hour forecast-'
     s['temperature'] = get_temperature_summary(data['main'])
     s['description'] = data['weather'][0]['description']
-    s['windspeed'] = 'windspeed: '+str(data['wind']['speed'])
-    return '\n'.join([s['header'], s['description'], s['temperature'], s['windspeed']])
+    return '\n'.join([s['header'], s['description'], s['temperature']])
 
 def get_forecast_weather_by_location(message):
     PAYLOAD = {'q': message.title(), 'type':'accurate','appid' : APP_ID, 'units':'imperial'}
@@ -60,11 +59,10 @@ def get_forecast_weather_by_location(message):
     else:
         return 'Sorry the location you sent is either not valid or forecast is not available for that area'
 
+def get_weather_by_location(message):
+    return get_current_weather_by_location(message)+get_forecast_weather_by_location(message)
+
 
 if __name__ == '__main__':
-    # print(len(get_current_weather_by_location('charlotte')))
-    # print(len(get_current_weather_by_location('vijayawada')))
-    import time
-    s = time.time()
-    print(len(get_current_weather_by_location('charlotte')+get_forecast_weather('charlotte')))
-    print('total time = ',time.time()-s)
+    print(get_weather_by_location('san jose'))
+    print(get_weather_by_location('vijayawada'))
