4f72bddb904c531c9a04b40b7237081712e78b4d
yamara <yklsga@gmail.com>
2017-11-13 11:27:22 -0800

    added conn to get

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def insert_into_redis(conn, details):
     conn.set(details.get_city, 'hahah')
     print('task done buoyyzz')
-    print('jilebi pakodi', r.get(details.get_city))
+    print('jilebi pakodi', conn.get(details.get_city))
